cefe63dd7088a40a6bd1afca7c5fa40932b808bf
Ammar Arif <evergreenkary@gmail.com>
2025-03-05 12:58:56 -0500

    build: use `dot` instead for cross compatibility

--- setup-scripts/setup-cairo.sh
+++ setup-scripts/setup-cairo.sh
@@ -7,5 +7,5 @@ pyenv local 3.9.15
 PYENV_VERSION=3.9.15 python -m venv snos-env
 
 # Activate the virtual environment and install the dependencies
-source snos-env/bin/activate
+. snos-env/bin/activate
 pip install -r requirements.txt
